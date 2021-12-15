86f3f5993f91fde83ec836e4ab9ba4d94d65a18b
agencyadmin <osmprobox@gmail.com>
2021-12-15 12:18:13 +0300

    задание 5.5.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programm
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Напишите что-то");
+
+        var str = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+
+        var deep = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < deep; i++)
+        {
+            Echo(str);
+        }
+
+
+        static void Echo(string saidword)
+        {
+            Console.WriteLine(saidword);
+        }
+    }
+}
+
