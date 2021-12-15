6e4e00008343089da37b64602c941c455eec9ac8
agencyadmin <osmprobox@gmail.com>
2021-12-15 12:32:29 +0300

    Задание 5.5.3. По заливке его на гит хаб. Программа эхо в рекурсии.

--- Program.cs
+++ Program.cs
@@ -19,9 +19,18 @@ class Programm
         }
 
 
-        static void Echo(string saidword)
+        static void Echo(string saidworld, int deep)
         {
-            Console.WriteLine(saidword);
+            var modif = saidworld;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
         }
     }
 }
