d517a7ef1328aaf7a1f3537d1a2f9dce23cd0bbf
agencyadmin <osmprobox@gmail.com>
2021-12-15 13:12:08 +0300

    Окрасил каждую новую строку новым цветом в рекурсии опираясь на объект рандом и создание совоего перечисления на базе транслируемого енума типа клавиатурных цветов .

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 
 class Programm
 {
+    static readonly Random random = new Random();
+    
     public static void Main(string[] args)
     {
+        
 
         Console.WriteLine("Напишите что-то");
 
@@ -21,11 +24,14 @@ class Programm
 
         static void Echo(string saidworld, int deep)
         {
+            var list = Enum.GetValues(typeof(ConsoleColor)).Cast<ConsoleColor>().ToArray();
             var modif = saidworld;
             if (modif.Length > 2)
             {
                 modif = modif.Remove(0, 2);
             }
+            int bcolor = random.Next(14);
+            Console.ForegroundColor = list[bcolor];
             Console.WriteLine("..." + modif);
             if (deep > 1)
             {
